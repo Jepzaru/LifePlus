14670ca873252311dc6ca961f5363709891a9c51
KmvrlyBcls <kimverly.bacalso@cit.edu>
2023-11-19 20:23:47 +0800

    origin

--- src/LifePages/Settings.js
+++ src/LifePages/Settings.js
@@ -1,16 +1,27 @@
-// Setting.js
-
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../LifeCss/Settings.css';
 import Sidenavbar from '../Life++/sidenavbar';
 import Header from '../Life++/Header';
+import { IoMdSettings } from "react-icons/io";
+import { FaUserEdit } from "react-icons/fa";
+import { MdDarkMode } from "react-icons/md";
+import { FaLink } from "react-icons/fa6";
+import { FaUnlock } from "react-icons/fa";
+
 
 function Setting() {
-  const [darkMode, setDarkMode] = useState(false);
+  // Check if dark mode preference is stored in localStorage
+  const savedDarkMode = localStorage.getItem('darkMode') === 'true';
+
+  const [darkMode, setDarkMode] = useState(savedDarkMode);
+
+  useEffect(() => {
+    // Update the localStorage value when dark mode changes
+    localStorage.setItem('darkMode', darkMode);
+  }, [darkMode]);
 
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
-    // You can save the user's preference in local storage or a state management system
   };
 
   return (
@@ -19,13 +30,16 @@ function Setting() {
       <Sidenavbar />
       <div className='set'>
         <h1>Settings</h1>
-        <div className="settings-container">
-          <div className="setting-button">Customize Profile</div>
-          <div className="setting-button" onClick={toggleDarkMode}>
+        <div className={`setitle ${darkMode ? 'dark-mode-title' : ''}`}>
+        <h1><IoMdSettings style={{ marginRight: '15px', marginBottom: '-5px', color: '#FF64B4' }}/>Settings</h1>
+      </div>
+      <div className={`settings-container ${darkMode ? 'dark-mode-container' : ''}`}>
+          <div className="setting-button"><FaUserEdit style={{ marginRight:'10px', marginBottom: '-2px'}}/>Customize Profile</div>
+          <div className="setting-button" onClick={toggleDarkMode}><MdDarkMode style={{ marginRight:'10px', marginBottom: '-2px'}} />
             {darkMode ? 'Light Mode' : 'Dark Mode'}
           </div>
-          <div className="setting-button">Link Social Account</div>
-          <div className="setting-button">Change Password</div>
+          <div className="setting-button"><FaLink style={{ marginRight:'10px', marginBottom: '-2px'}}/>Link Social Account</div>
+          <div className="setting-button"><FaUnlock style={{ marginRight:'10px', marginBottom: '-2px'}}/>Change Password</div>
         </div>
       </div>
     </div>
