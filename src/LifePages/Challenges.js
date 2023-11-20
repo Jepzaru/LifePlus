3c354fdbacc7fb3963649138637d9f5ab3608fef
KmvrlyBcls <kimverly.bacalso@cit.edu>
2023-11-20 16:41:08 +0800

    origin

--- src/LifePages/Challenges.js
+++ src/LifePages/Challenges.js
@@ -6,6 +6,10 @@ import Header from '../Life++/Header';
 import { IoExtensionPuzzle } from 'react-icons/io5';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import armsImage from '../LifeImages/arms.png';
+import absImage from '../LifeImages/abs.png';
+import cardioImage from '../LifeImages/cardio.png';
+import strengthImage from '../LifeImages/strength.png';
 
 function Challenges() {
   // Configure the settings for the slider
@@ -28,45 +32,49 @@ function Challenges() {
         <h1><IoExtensionPuzzle style={{ marginRight: '15px', marginBottom: '-5px', color: '#FF64B4' }} />Challenges</h1>
       </div>
       <div className='phy-cha'>
-        <h2>Physical Challenges</h2>
-        <Slider {...sliderSettings}>
-            {/* Card 1 */}
-            <div>
-                <div className="card-content">
-                <h3>Card 1 Content</h3>
+    <h2>Physical Challenges</h2>
+    <Slider {...sliderSettings}>
+        {/* Card 1 */}
+        <div>
+            <div className="card-content">
+                <h3>Arms Challenge</h3>
+                <img src={armsImage} alt="Arms Challenge" />
                 <div className='viewcha'>
-              <button className='phy-viewcha-button'>View Challenge</button>
-            </div>
+                    <button className='phy-viewcha-button'>View Challenge</button>
                 </div>
             </div>
-            {/* Card 2 */}
-            <div>
-                <div className="card-content">
-                <h3>Card 2 Content</h3>
+        </div>
+        {/* Card 2 */}
+        <div>
+            <div className="card-content">
+                <h3>Abs Challenge</h3>
+                <img src={absImage} alt="Abs Challenge" /> {/* Image for Leg Challenge */}
                 <div className='viewcha'>
-              <button className='phy-viewcha-button'>View Challenge</button>
-            </div>
+                    <button className='phy-viewcha-button'>View Challenge</button>
                 </div>
             </div>
-            {/* Card 3 */}
-            <div>
-                <div className="card-content">
-                <h3>Card 3 Content</h3>
+        </div>
+        {/* Card 3 */}
+        <div>
+            <div className="card-content">
+                <h3>Cardio Challenge</h3>
+                <img src={cardioImage} alt="Cardio Challenge" /> {/* Image for Cardio Challenge */}
                 <div className='viewcha'>
-              <button className='phy-viewcha-button'>View Challenge</button>
-            </div>
+                    <button className='phy-viewcha-button'>View Challenge</button>
                 </div>
             </div>
-            <div>
-                <div className="card-content">
-                <h3>Card 4 Content</h3>
+        </div>
+        {/* Card 4 */}
+        <div>
+            <div className="card-content">
+                <h3>Strength Challenge</h3>
+                <img src={strengthImage} alt="Strength Challenge" /> {/* Image for Strength Challenge */}
                 <div className='viewcha'>
-              <button className='phy-viewcha-button'>View Challenge</button>
-            </div>
+                    <button className='phy-viewcha-button'>View Challenge</button>
+                </div>
                 </div>
             </div>
-            </Slider>
-            
+          </Slider>
       </div>
       <div className='ment-cha'>
         <h2>Mental Challenges</h2>
