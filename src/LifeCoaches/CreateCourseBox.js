0a153055f11936b1f412be2ed978465d6c2d6189
Jepzaru <jefffrancis.conson@cit.edu>
2023-12-04 21:51:26 +0800

    origin

--- src/LifeCoaches/CreateCourseBox.js
+++ src/LifeCoaches/CreateCourseBox.js
@@ -2,10 +2,71 @@ import React, { useEffect, useState } from 'react';
 import '../LifeCss/Courses.css';
 import image1 from '../LifeImages/1.jpg';
 import image2 from '../LifeImages/men1.jpg';
+import { useAuth } from '../Life++/AuthContext';
+import axios from 'axios';
 
 const CreateCourseBox = ({ onClose }) => {
+  const { user } = useAuth();
   const [isOutro, setIsOutro] = useState(false);
-  const [selectedImage, setSelectedImage] = useState(image1); // Set the initial selected image
+  const [selectedImage, setSelectedImage] = useState(image1);
+  const [courseData, setCourseData] = useState({
+    description: '',
+    max: '',
+    name: '',
+    coach: null,
+  });
+
+  useEffect(() => {
+    const fetchCoachData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/coach/get');
+        const coaches = response.data; 
+
+        const foundCoach = coaches.find(coach => coach.username === user.username);
+
+        if (foundCoach) {
+          setCourseData(prevData => ({ ...prevData, coach: foundCoach }));
+          console.log('Found Coach:', foundCoach); 
+        } else {
+          console.log('Coach not found for the user');
+        }
+      } catch (error) {
+        console.error('Error fetching coach data:', error);
+      }
+    };
+
+    fetchCoachData();
+  }, [user.username]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    const parsedValue = name === 'max' ? parseInt(value, 10) : value;
+
+    setCourseData((prevData) => ({
+      ...prevData,
+      [name]: parsedValue,
+    }));
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const newCourse = {
+        description: courseData.description,
+        max: parseInt(courseData.max, 10),
+        name: courseData.name,
+        coach: courseData.coach,
+      };
+
+      const response = await axios.post('http://localhost:8080/course/insert', newCourse);
+      // Handle successful response if needed
+      console.log('New course created:', response.data);
+      // Close the create course box or perform other actions
+      onClose();
+    } catch (error) {
+      // Handle errors if the request fails
+      console.error('Error creating course:', error);
+    }
+  };
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
@@ -33,44 +94,56 @@ const CreateCourseBox = ({ onClose }) => {
         <button className='create-course-close' onClick={handleOutro}>Cancel</button>
       </div>
       <div className='create-course-con'>
-      <div className='image-selector'>
-        <h3>Select Course Image</h3>
-        <div className='image-options'>
-          <img
-            src={image1}
-            alt="1"
-            onClick={() => handleImageClick(image1)}
-            className={selectedImage === image1 ? 'selected' : ''}
-          />
-          <img
-            src={image2}
-            alt="2"
-            onClick={() => handleImageClick(image2)}
-            className={selectedImage === image2 ? 'selected' : ''}
-          />
-        </div>
-        {selectedImage && (
-          <div>
-            <h4>Selected Course Image:</h4>
-            <img src={selectedImage} alt="Selected Course Image" />
+        <div className='image-selector'>
+          <h3>Select Course Image</h3>
+          <div className='image-options'>
+            <img
+              src={image1}
+              alt="1"
+              onClick={() => handleImageClick(image1)}
+              className={selectedImage === image1 ? 'selected' : ''}
+            />
+            <img
+              src={image2}
+              alt="2"
+              onClick={() => handleImageClick(image2)}
+              className={selectedImage === image2 ? 'selected' : ''}
+            />
           </div>
-        )}
-      </div>
+          {selectedImage && <img src={selectedImage} alt="Selected Course Image" />}
+        </div>
         <div className='course-name-tit'>
-        <h3>Course Title</h3>
-        <input type="text" />
+          <h3>Course Title</h3>
+          <input
+            type="text"
+            value={courseData.name}
+            onChange={(e) => handleInputChange(e)}
+            name="name"
+          />
         </div>
         <div className='course-des'>
           <h3>Course Description</h3>
-          <textarea></textarea>
+          <textarea
+            value={courseData.description}
+            onChange={(e) => handleInputChange(e)}
+            name="description"
+          ></textarea>
         </div>
         <div className='course-capa-max'>
           <h3>Course Maximum Capacity</h3>
-          <input className='max-capa' type="text" />
+          <input
+            className='max-capa'
+            type="text"
+            value={courseData.max}
+            onChange={(e) => handleInputChange(e)}
+            name="max"
+          />
         </div>
-    
+
         <div className='create-course-save'>
-          <button className='create-save'>Create Course</button>
+          <button className='create-save' onClick={handleSubmit}>
+            Create Course
+          </button>
         </div>
       </div>
     </div>
