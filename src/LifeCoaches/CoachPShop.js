211e96c84849d3bf2c619f78d74858eba81c97c0
KmvrlyBcls <kimverly.bacalso@cit.edu>
2023-12-08 19:00:32 +0800

    origin

--- src/LifeCoaches/CoachPShop.js
+++ src/LifeCoaches/CoachPShop.js
@@ -1,26 +1,59 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import HashLoader from 'react-spinners/HashLoader'; 
+import StoreIcon from '@mui/icons-material/Store'; 
 import CoachSidenavbar from '../Life++/coachsidebar';
 import CoachHeader from '../Life++/CoachHeader';
-import { useAuth } from '../Life++/AuthContext'; // Import useAuth
+import { useAuth } from '../Life++/AuthContext';
 
-function CoachPShop() {
-  const { login } = useAuth(); // Get login function from useAuth
+function CoachShop() {
+  const { login } = useAuth(); 
+  const [loading, setLoading] = useState(false);
+  const savedDarkMode = localStorage.getItem('darkMode') === 'true';
+  const [darkMode] = useState(savedDarkMode)
 
   useEffect(() => {
-    // Load user from localStorage on component mount
     const storedUser = JSON.parse(localStorage.getItem('loggedInUser'));
     if (storedUser) {
       login(storedUser);
     }
   }, [login]);
 
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('darkMode', darkMode);
+  }, [darkMode]);
+
   return (
-    <div className="appindex">
-      <CoachHeader />
-      <CoachSidenavbar />
-      {/* ... rest of the component code ... */}
+    <div className={`appincoach ${darkMode ? 'dark-mode' : ''}`}>
+  {loading ? (
+    <div className="hash">
+      <HashLoader size={100} color={'#FF64B4'} loading={loading} />
     </div>
-  );
+  ) : (
+    <>
+      <div className="appindex">
+        <CoachHeader />
+        <CoachSidenavbar />
+        <div className='coa'>
+          <h1>Point Shop</h1>
+        </div>
+        <div className={`coa-title${darkMode ? ' dark-mode-title' : ''}`}>
+          <h1><StoreIcon style={{ fontSize: '4rem',marginRight: '15px', marginBottom: '-15px', color: '#FF64B4' }}/>Point Shop</h1>
+        </div> 
+        <div className='coa-container'>
+          {/* ... rest of the code ... */}
+        </div>
+      </div>
+    </>
+  )}
+</div>
+);
 }
 
-export default CoachPShop;
+export default CoachShop;
--- src/LifeCoaches/CoachQuest.js
+++ src/LifeCoaches/CoachQuest.js
@@ -1,26 +1,59 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import HashLoader from 'react-spinners/HashLoader'; 
+import SportsIcon from '@mui/icons-material/Sports'; 
 import CoachSidenavbar from '../Life++/coachsidebar';
 import CoachHeader from '../Life++/CoachHeader';
-import { useAuth } from '../Life++/AuthContext'; // Import useAuth
+import { useAuth } from '../Life++/AuthContext';
 
 function CoachQuest() {
-  const { login } = useAuth(); // Get login function from useAuth
+  const { login } = useAuth(); 
+  const [loading, setLoading] = useState(false);
+  const savedDarkMode = localStorage.getItem('darkMode') === 'true';
+  const [darkMode] = useState(savedDarkMode)
 
   useEffect(() => {
-    // Load user from localStorage on component mount
     const storedUser = JSON.parse(localStorage.getItem('loggedInUser'));
     if (storedUser) {
       login(storedUser);
     }
   }, [login]);
 
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('darkMode', darkMode);
+  }, [darkMode]);
+
   return (
-    <div className="appindex">
-      <CoachHeader />
-      <CoachSidenavbar />
-      {/* ... rest of the component code ... */}
+    <div className={`appincoach ${darkMode ? 'dark-mode' : ''}`}>
+  {loading ? (
+    <div className="hash">
+      <HashLoader size={100} color={'#FF64B4'} loading={loading} />
     </div>
-  );
+  ) : (
+    <>
+      <div className="appindex">
+        <CoachHeader />
+        <CoachSidenavbar />
+        <div className='coa'>
+          <h1>Quest</h1>
+        </div>
+        <div className={`coa-title${darkMode ? ' dark-mode-title' : ''}`}>
+          <h1><SportsIcon style={{ fontSize: '4rem',marginRight: '15px', marginBottom: '-15px', color: '#FF64B4' }}/>Quest</h1>
+        </div> 
+        <div className='coa-container'>
+          {/* ... rest of the code ... */}
+        </div>
+      </div>
+    </>
+  )}
+</div>
+);
 }
 
 export default CoachQuest;
