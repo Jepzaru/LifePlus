83d06fcf5b47238ff79ca9103d37ab1c178124df
Jepzaru <jefffrancis.conson@cit.edu>
2023-12-16 19:46:00 +0800

    origin

--- src/LifeCoaches/CoachCourse.js
+++ src/LifeCoaches/CoachCourse.js
@@ -19,10 +19,6 @@ import { FaScroll } from "react-icons/fa6";
 import { IoMdAddCircle } from "react-icons/io";
 import axios from 'axios';
 
-
-
-
-
 function CoachCourses() {
   const { login } = useAuth();
   const [storedUser, setStoredUser] = useState(null);
@@ -71,8 +67,6 @@ function CoachCourses() {
     }
   }, [login, storedUser]);
 
-
-
   useEffect(() => {
     setLoading(true);
     if (coachData && coachData.coachid) {
@@ -97,7 +91,6 @@ function CoachCourses() {
     }
   }, [coachData]);
 
-
   const fetchQuestsForCourse = async (courseId) => {
     try {
       const response = await axios.get(`http://localhost:8080/course/${courseId}/getquests`);
@@ -110,8 +103,7 @@ function CoachCourses() {
   };
 
   const handleRemoveCourse = (courseId) => {
-    const headers = {
-    };
+    const headers = {};
 
     axios.delete(`http://localhost:8080/course/delete/${courseId}`, { headers })
       .then(response => {
@@ -149,7 +141,6 @@ function CoachCourses() {
     }
   }, []);
 
-
   return (
     <div className={`appindcourse ${darkMode ? 'dark-mode' : ''}`}>
       {loading ? (
@@ -183,7 +174,6 @@ function CoachCourses() {
                       <img src={index % 2 === 0 ? image1 : image2} alt={`Course ${course.name}`} className='course-image'
                         style={{ height: '300px', width: '300px', marginLeft: '20px', borderRadius: '15px' }}
                       />
-
                     </div>
                     <div className='Cname'>{course.name}  <IoMdAddCircle
                       style={{ color: 'green', marginLeft: '20px', cursor: 'pointer' }}
@@ -197,9 +187,7 @@ function CoachCourses() {
                     <div className='delete-cou'><button onClick={() => handleRemoveCourse(course.courseID)}>Remove Course</button>
                     </div>
                   </div>
-
                 </div>
-
               ))}
           </div>
           <div className="up-act">
@@ -212,8 +200,8 @@ function CoachCourses() {
             <div className='created-quest'>
               {courses.map(course => (
                 <div key={course.id} className='quest-list'>
-                  {course.quests && course.quests.length == 0 ? (
-                    console.log("None")
+                  {course.quests && course.quests.length === 0 ? (
+                    <p>None</p>
                   ) : (
                     <h3>{course.name}</h3>
                   )}
@@ -227,11 +215,9 @@ function CoachCourses() {
                     <p>No quests available for this course</p>
                   )}
                 </div>
-              </div>
+              ))}
             </div>
           </div>
-         
-
           <Snackbar
             open={snackbar.open}
             autoHideDuration={6000}
@@ -263,7 +249,6 @@ function CoachCourses() {
           course={showViewMembersBox} // Pass course object here
         />
       )}
-
     </div>
   );
 }
